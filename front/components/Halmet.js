af683d79d0da2ef2a6471878a25ee4f6bf95a18b
Hailey_ys <whassup_man@naver.com>
2022-06-06 22:11:31 +0900

    Add Helmet

---
+++ front/components/Halmet.js
@@ -0,0 +1,9 @@
+import Head from 'next/head';
+
+export default function Halmet({ title }) {
+  return (
+    <Head>
+      <title>{title} | 의자왕을 찾아라👑</title>
+    </Head>
+  );
+}
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,3 +1,10 @@
+import Halmet from '../components/Halmet';
+
 export default function Home() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <div>
+      <Halmet title='HOME' />
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    </div>
+  );
 }
