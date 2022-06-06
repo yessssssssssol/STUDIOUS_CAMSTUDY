aaae4596d7971de44c3b47cc230800a04e37f320
Hailey_ys <whassup_man@naver.com>
2022-06-06 22:11:31 +0900

    Add Halmet

---
+++ front/components/ProfileCard.js
@@ -0,0 +1,9 @@
+export default function ProfileCard() {
+  return (
+    <div class='max-w-sm rounded overflow-hidden shadow-lg'>
+      <img img class='w-full' src='/img/card-top.jpg' alt='Sunset in the mountains'></img>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
