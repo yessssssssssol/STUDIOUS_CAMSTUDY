7a7eb4b51d7bc7852fc391952611091219d56d14
vjvl95 <vjvl95@naver.com>
2022-06-07 09:32:44 +0900

    Nav and Button

---
+++ front/components/Button.js
@@ -0,0 +1,8 @@
+export default function Button({Text}){
+
+    return (
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        {Text}
+        </button>
+    )
+}
---
+++ front/components/NavBar.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export default function NavBar(){
+    return(
+    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+    <div class="container flex flex-wrap justify-between items-center mx-auto">
+    <Link href="https://flowbite.com"><a class="flex items-center">
+        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">의자왕</span>
+    </a>
+    </Link>
+    <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
+      <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+        <li>
+          <Link href="/"><a class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">스터디 모집</a></Link>
+        </li>
+        <li>
+        <Link href="/"><a class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">마이페이지</a></Link>
+        </li>
+        <li>
+        <Link href="/"><a class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">추가 메뉴</a></Link>
+        </li>
+         <li>
+        <Link href="/"><a class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a></Link>
+        </li>
+      </ul>
+    </div>
+    </div>
+  </nav>
+  )
+}
