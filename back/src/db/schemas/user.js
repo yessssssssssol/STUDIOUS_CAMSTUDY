ae0f89d55b383b5de7caa89a80c80c591f8f9b40
kunnie-code <kearnyyoon@gmail.com>
2022-06-07 12:05:50 +0900

    [replace] change time log in UserSchema

--- back/src/db/schemas/user.js
+++ back/src/db/schemas/user.js
@@ -1,4 +1,8 @@
 import { Schema, model } from "mongoose";
+import dayjs from 'dayjs' 
+
+const date = dayjs()
+// console.log(date.format("YYYY-MM-DD HH:mm:ss"))
 
 const UserSchema = new Schema(
   {
@@ -23,10 +27,26 @@ const UserSchema = new Schema(
       required: false,
       default: "설명이 아직 없습니다. 추가해 주세요.",
     },
+    // 하루 공부할 목표 시간 / 분단위로 들어오게
+    // timeGoalADay: {
+    //   type: Number,
+    //   default: 0,
+    //   required: false,
+    // },
+    createdAt: {
+      type: String,
+      required: true,
+      default: date.format("YYYY-MM-DD HH:mm:ss")
+    },
+    updatedAt: {
+      type: String,
+      required: true,
+      default: date.format("YYYY-MM-DD HH:mm:ss")
+    }
   },
-  {
-    timestamps: true,
-  }
+  // {
+  //   timestamps: true,
+  // }
 );
 
 const UserModel = model("User", UserSchema);
