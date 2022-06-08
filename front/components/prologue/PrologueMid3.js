a9e96c96519a8fb79d46969b8554bb22ebdc84f1
ys <lhyahoo0119@gmail.com>
2022-06-09 00:11:24 +0900

    Add Prologue components

---
+++ front/components/prologue/PrologueMid3.js
@@ -0,0 +1,33 @@
+const PrologueMid3 = () => {
+  return (
+    <div class='container px-6 py-16 mx-auto'>
+      <div class='items-center lg:flex'>
+        <div class='w-full lg:w-1/2'>
+          <div class='lg:max-w-lg'>
+            <h1 class='text-2xl font-semibold text-gray-800 uppercase dark:text-white lg:text-3xl'>
+              Best Place To Choose Your Clothes
+            </h1>
+            <p class='mt-2 text-gray-600 dark:text-gray-400'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro
+              beatae error laborum ab amet sunt recusandae? Reiciendis natus
+              perspiciatis optio.
+            </p>
+            <button class='w-full px-3 py-2 mt-6 text-xs font-medium text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500'>
+              Shop Now
+            </button>
+          </div>
+        </div>
+
+        <div class='flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2'>
+          <img
+            class='w-full h-full lg:max-w-2xl'
+            src='https://images.unsplash.com/photo-1579586337278-3befd40fd17a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+            alt='Catalogue-pana.svg'
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PrologueMid3;
--- front/pages/prologue/index.js
+++ front/pages/prologue/index.js
@@ -1,5 +1,19 @@
+import PrologueBottom from '../../components/prologue/PrologueBottom';
+import PrologueMid from '../../components/prologue/PrologueMid';
+import PrologueMid2 from '../../components/prologue/PrologueMid2';
+import PrologueMid3 from '../../components/prologue/PrologueMid3';
+import PrologueTop from '../../components/prologue/PrologueTop';
+
 const Prologue = () => {
-  return <div></div>;
+  return (
+    <div>
+      <PrologueTop />
+      <PrologueMid />
+      <PrologueMid2 />
+      <PrologueMid3 />
+      <PrologueBottom />
+    </div>
+  );
 };
 
 export default Prologue;
