e92e717b18ef21adab87941281b5b3a187a7a7bf
Hailey_ys <whassup_man@naver.com>
2022-06-08 11:32:08 +0900

    Fix ProfileCard

--- front/components/Halmet.js
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head';
-
-export default function Halmet({ title }) {
-  return (
-    <Head>
-      <title>{title} | 의자왕을 찾아라👑</title>
-    </Head>
-  );
-}
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,10 +1,10 @@
-import Halmet from '../components/Halmet';
+import Helmet from '../components/Helmet';
 import ProfileCard from '../components/ProfileCard';
 
 export default function Home() {
   return (
     <div>
-      <Halmet title='HOME' />
+      <Helmet title='HOME' />
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
       <ProfileCard />
     </div>
