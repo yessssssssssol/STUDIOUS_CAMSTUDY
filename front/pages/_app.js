8565e159530a788a337fd06d1b88f960db0d0db6
vjvl95 <vjvl95@naver.com>
2022-06-08 22:08:51 +0900

    Nav 드롭박스 가능

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,12 +1,11 @@
 import '../styles/globals.css';
-import NavBarIcon from '../components/NavBarIcon';
 
 import Button from '../components/Button';
+import NavBarIcon from '../components/NavBar';
 function MyApp({ Component, pageProps }) {
  
   return   ( 
   <div>
-
     <NavBarIcon/>
     <Component {...pageProps} />
     <Button Text={"스터디 시작하기"}/>
