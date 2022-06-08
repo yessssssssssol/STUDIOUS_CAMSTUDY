61b984359939dc4b541bfed730b9cb22877bde8a
vjvl95 <vjvl95@naver.com>
2022-06-08 18:28:04 +0900

    Nav 완성

---
+++ front/components/NavBarIcon.js
@@ -0,0 +1,58 @@
+import Link from "next/link";
+
+export default function NavBarIcon(){
+    const items=["스터디 모집","마이페이지","추가 메뉴", "프롤로그"]
+    const drop_item=["Dashboard","Settings","Earnings","Sign out"]
+    function NavItem(item,index){
+        return(
+            <li key={index}>
+          <Link href="/"><a class="block py-2 mx-20 font-semibold pr-4 pl-3 text-gray-1000 border-gray-300 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{item}</a></Link>
+        </li>
+        )
+    }
+    function NavDropItem(item,index){
+        return(
+            <li key={index}>
+          <Link href="/"><a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">{item}</a></Link>
+        </li>
+        )
+    }
+    return (
+        <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+  <div class="container flex flex-wrap justify-between items-center mx-auto">
+  <Link href="https://flowbite.com"><a class="flex items-center">
+        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">의자왕</span>
+    </a></Link>
+  <div class="flex items-center md:order-2">
+      <button type="button" class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" type="button" data-dropdown-toggle="dropdown">
+        <span class="sr-only">Open user menu</span>
+        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="user photo"/>
+      </button>
+      <div class="hidden z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown">
+        <div class="py-3 px-4">
+          <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+          <span class="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+        </div>
+        <ul class="py-1" aria-labelledby="dropdown">
+        {
+            drop_item.map((item,index)=>NavDropItem(item,index))
+        }
+        </ul>
+      </div>
+      <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+      <span class="sr-only">Open main menu</span>
+      <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+      <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+    </button>
+  </div>
+  <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
+    <ul class="flex flex-col mt-4  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+        {
+            items.map((item,index)=>NavItem(item,index))
+        }
+    </ul>
+  </div>
+  </div>
+</nav>
+    )
+}
--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,12 +1,13 @@
 import '../styles/globals.css';
-import NavBar from '../components/NavBar';
+import NavBarIcon from '../components/NavBarIcon';
+
 import Button from '../components/Button';
 function MyApp({ Component, pageProps }) {
  
   return   ( 
   <div>
 
-    <NavBar/>
+    <NavBarIcon/>
     <Component {...pageProps} />
     <Button Text={"스터디 시작하기"}/>
   </div>
