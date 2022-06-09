431ae30db2b4930bb110c5efe72c45b0ecb9dd1b
Hailey_ys <whassup_man@naver.com>
2022-06-09 11:26:58 +0900

    Add board ver2

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,7 +1,15 @@
+import { RecoilRoot } from 'recoil';
 import '../styles/globals.css';
-
+import NavBar from '../components/layout/NavBar';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <RecoilRoot>
+        <NavBar />
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </div>
+  );
 }
 
 export default MyApp;
