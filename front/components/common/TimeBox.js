a47a4736a97fedefcacd1fb8e599b771c7bfa2dd
vjvl95 <vjvl95@naver.com>
2022-06-11 00:17:50 +0900

    MyPage 완성 및 수정

--- front/components/common/TimeBox.js
+++ front/components/common/TimeBox.js
@@ -1,6 +1,14 @@
-export default function TimeBox({Title})
+import {useEffect} from "react"
+
+
+
+export default function TimeBox({Title,color})
 {
-    return(<div class="bg-gray-300 w-[320px] h-[125px] mt-[30px]">
+    useEffect(()=>{
+        console.log(color)
+    },[])
+    
+    return(<div class={`bg-green-100 w-[320px] h-[125px] mt-[30px]`}>
         <div class="font-bold pt-[10px] text-center">{Title}</div>
         <div class="text-center text-5xl pt-[10px]">00:40:20</div>
     </div>)
--- front/pages/mypage.js
+++ front/pages/mypage.js
@@ -2,20 +2,26 @@ import BoldText from '../components/common/BoldText'
 import TimeBox from '../components/common/TimeBox'
 
 import Pie from '../components/common/Pie'
+import { useEffect } from 'react'
 export default function mypage()
 {
-    
     const charts_data=[["출석율","#ffefd5"],["목표 달성률","#8ee69a"],["일일 최다 공부시간","#b999f3"]]
-    
+    const Title_time=["오늘 공부 시간","이번주 공부 시간","전체 공부 시간"]
+    const randomColor=["red","blue","green"]
+
+    useEffect(()=>{
+        console.log(Math.ceil((Math.random()*15)%14))
+    },[])
     return(
     <div class="px-[300px] py-[50px]">
         <div class="min-w-[350px]">
         <BoldText Text={"주완님의 최근 공부시간"}/>
         </div>
         <div class="flex justify-between min-w-[1000px]">
-        <TimeBox Title={"오늘 공부 시간"}></TimeBox>
-        <TimeBox Title={"이번주 공부 시간"}></TimeBox>
-        <TimeBox Title={"전체 공부 시간"}></TimeBox>
+        {
+            Title_time.map((title,index)=> <TimeBox Title={title} color={randomColor[index]}/>)
+        }
+        
         </div>
         <div class="min-w-[350px] pt-[50px]">
         <BoldText Text={"주완님의 공부기록"}/>
