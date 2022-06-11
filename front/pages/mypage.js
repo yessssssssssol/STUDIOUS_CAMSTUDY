ee56dd23ead5f51f63ab3aecfe5471ce37cd3bbf
vjvl95 <vjvl95@naver.com>
2022-06-11 17:16:29 +0900

    git 잔디심기 최신

--- front/pages/mypage.js
+++ front/pages/mypage.js
@@ -1,26 +1,19 @@
 import BoldText from '../components/common/BoldText'
 import TimeBox from '../components/common/TimeBox'
-import CalendarHeatmap from 'react-calendar-heatmap';
-import 'react-calendar-heatmap/dist/styles.css';
-
+import dynamic from "next/dynamic";
 import Pie from '../components/common/Pie'
+
 export default function mypage()
 {
     const charts_data=[["출석율","#ffefd5"],["목표 달성률","#8ee69a"],["일일 최다 공부시간","#b999f3"]]
     const Title_time=["오늘 공부 시간","이번주 공부 시간","전체 공부 시간"]
     const randomColor=["red","blue","green"]
-    var data = [{
-        "date": "2016-01-01",
-        "total": 17164,
-        "details": [{
-          "name": "Project 1",
-          "date": "2016-01-01 12:30:45",
-          "value": 9192
-        }, {
-          "name": "Project 2",
-          "date": "2016-01-01 13:37:00",
-          "value": 6753
-        }]}]
+    const NoSSR = dynamic(
+        () =>import("../components/common/Heatmap"),
+        {
+          ssr: false,
+        }
+      );
     return(
     <div class="flex-col px-[300px] py-[50px]">
         <div class="min-w-[350px]">
@@ -32,24 +25,12 @@ export default function mypage()
         }
         
         </div>
-        <div class="pt-[50px]">
-        <BoldText Text={"주완님의 공부기록"}/>
-                <div class="flex justify-between min-w-[1000px] pt-[30px]" >
-                <CalendarHeatmap
-                    startDate={new Date('2022-01-01')}
-                    endDate={new Date('2022-12-30')}
-                    gutterSize={1}
-                    horizontal={true}
-                    values={[
-                        { date: '2022-01-02', count: 5 },
-                        { date: '2022-01-22', count: 122 },
-                        { date: '2022-01-29', count: 38 },
-                        { date: '2022-01-15', count: 12 },
-                        { date: '2022-01-17', count: 122 },
-                        { date: '2022-01-28', count: 38 },
-                    ]}
-                    />
-                </div>
+        <div class=" pt-[50px]">
+        <BoldText Text={"주완님의 공부기록"}/>    
+        <div class="pt-[10px]">
+
+        <NoSSR ></NoSSR>
+        </div>
         </div>
 
         <div class="min-w-[350px] pt-[50px]">
