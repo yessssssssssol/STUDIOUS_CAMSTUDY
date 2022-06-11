77f61b502283549a407cc8ae0ce280a34ac94c8f
ys <lhyahoo0119@gmail.com>
2022-06-12 00:00:31 +0900

    Add webacm files

--- front/components/studyroon/MyWebcam.js
+++ front/components/studyroon/MyWebcam.js
@@ -1,20 +1,44 @@
+// const MyWebcam = () => {
+//   const getWebcam = (callback) => {
+//     try {
+//       const constraints = {
+//         video: true,
+//         audio: false,
+//       };
+//       navigator.mediaDevices.getUserMedia(constraints).then(callback);
+//     } catch (err) {
+//       console.log(err);
+//       return undefined;
+//     }
+//   };
+
+//   return (
+//     <div className='flex justify-center'>
+//       <video className='w-screen h-screen' autoPlay />
+//     </div>
+//   );
+// };
+
+// export default MyWebcam;
+
+import { useRef } from 'react';
+
 const MyWebcam = () => {
-  const getWebcam = (callback) => {
-    try {
-      const constraints = {
-        video: true,
-        audio: false,
-      };
-      navigator.mediaDevices.getUserMedia(constraints).then(callback);
-    } catch (err) {
-      console.log(err);
-      return undefined;
+  const CONSTRAINTS = { video: true };
+
+  const videoRef = useRef(null);
+
+  const startVideo = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia(CONSTRAINTS);
+    if (videoRef && videoRef.current && !videoRef.current.srcObject) {
+      videoRef.current.srcObject = stream;
     }
   };
 
   return (
     <div className='flex justify-center'>
-      <video className='w-screen h-screen' autoPlay />
+      <video className='' autoPlay ref={videoRef} />
+      <button onClick={startVideo}>start</button>
     </div>
   );
 };
