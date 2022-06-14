9b2ece771943e4a413415fded2c48334a7664a1c
Hailey_ys <whassup_man@naver.com>
2022-06-15 00:21:21 +0900

    Add Editpage

--- front/components/layout/NavBar.js
+++ front/components/layout/NavBar.js
@@ -1,73 +1,111 @@
-import Link from "next/link";
-import { useEffect,useState } from "react";
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import LoginModal from '../user/LoginModal';
 import RegisterModal from '../user/RegisterModal';
-import {useRouter} from 'next/router'
-export default function NavBar(){
+import { useRouter } from 'next/router';
+export default function NavBar() {
+  const router = useRouter();
+  const currentRoute = router.pathname;
 
-    const router = useRouter();
-    const currentRoute = router.pathname;
-
-    const [showOptions, setShowOptions] = useState(false)
-    const [Islogin,setIslogin]=useState(true)
-    const handleShow = () => {
-        setShowOptions(!showOptions)
-    }  
-    useEffect( ()=>{console.log(currentRoute)},[currentRoute])
-    const items=[["스터디 모집","/board" ],["마이페이지","/mypage"],["AboutUs","/aboutus"],["프롤로그","/prologue"]]
-    const drop_item=["Dashboard","Settings","Earnings","Sign out"]
-    function NavItem(item,index){
-        return(
-        <li key={index}>
-          <Link href={item[1]}><a style={{color : router.pathname===item[1] ? "blue": "black"}}class={`block py-2 px-5 pr-4 pl-3 mx-20 border-b  hover:bg-gray-50  border-0  p-0`}>{item[0]}</a></Link>
-        </li>
-        )
-    }
-    function NavDropItem(item,index){
-        return(
-            <li key={index}>
-          <Link href="/"><a class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">{item}</a></Link>
-        </li>
-        )
-    }
+  const [showOptions, setShowOptions] = useState(false);
+  const [Islogin, setIslogin] = useState(false);
+  const handleShow = () => {
+    setShowOptions(!showOptions);
+  };
+  useEffect(() => {
+    console.log(currentRoute);
+  }, [currentRoute]);
+  const items = [
+    ['스터디 모집', '/board'],
+    ['마이페이지', '/mypage'],
+    ['AboutUs', '/aboutus'],
+    ['프롤로그', '/prologue'],
+  ];
+  const drop_item = ['Dashboard', 'Settings', 'Earnings', 'Sign out'];
+  function NavItem(item, index) {
     return (
-<nav class="bg-white border-gray-200 px-2 py-5 rounded min-w-[1000px]">
-  <div class="container flex flex-wrap justify-between items-center mx-auto min-w-[1550px]">
-  <Link href="/">
-  <a class="flex items-center">
-      <span class="self-center text-xl font-semibold whitespace-nowrap">의자왕👑</span>
-  </a>
-  </Link>
-  
-  <div class="items-center flex w-auto order-1">
-    <ul class="flex  mt-4 flex-row space-x-8 mt-0 text-sm font-medium">
-      {
-            items.map((item,index)=>NavItem(item,index))
-    }
-    </ul>
-    {
-        Islogin===true ? (<div class="relative flex items-center md:order-2">
-        <button onClick={handleShow} type="button" class="inline-flex justify-center w-full mx-20 rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
-              <span class="sr-only">Open user menu</span>
-              <img class="w-8 h-8 rounded-full" src="favicon.ico" alt="user photo"/>
-            </button>
-            {showOptions && (
-            <div class="absolute top-9 z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow" id="dropdown">
-              <div class="py-3 px-4" >
-                <span class="block text-sm text-gray-900">Bonnie Green</span>
-                <span class="block text-sm font-medium text-gray-500 truncate">name@flowbite.com</span>
-              </div>
-              <ul class="py-1" aria-labelledby="dropdown" >
-              {
-                  drop_item.map((item,index)=>NavDropItem(item,index))
-              }
-              </ul>
-            </div>)}
-        </div>) : ( <><LoginModal /><RegisterModal /></>)
-    }
-         
-  </div>
-  </div>
-</nav>
-    )
+      <li key={index}>
+        <Link href={item[1]}>
+          <a
+            style={{ color: router.pathname === item[1] ? 'blue' : 'black' }}
+            class={`block py-2 px-5 pr-4 pl-3 mx-20 border-b  hover:bg-gray-50  border-0  p-0`}
+          >
+            {item[0]}
+          </a>
+        </Link>
+      </li>
+    );
+  }
+  function NavDropItem(item, index) {
+    return (
+      <li key={index}>
+        <Link href="/">
+          <a class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">
+            {item}
+          </a>
+        </Link>
+      </li>
+    );
+  }
+  return (
+    <nav class="bg-white border-gray-200 px-2 py-5 rounded min-w-[1000px]">
+      <div class="container flex flex-wrap justify-between items-center mx-auto min-w-[1550px]">
+        <Link href="/">
+          <a class="flex items-center">
+            <span class="self-center text-xl font-semibold whitespace-nowrap">
+              의자왕👑
+            </span>
+          </a>
+        </Link>
+
+        <div class="items-center flex w-auto order-1">
+          <ul class="flex  mt-4 flex-row space-x-8 mt-0 text-sm font-medium">
+            {items.map((item, index) => NavItem(item, index))}
+          </ul>
+          {Islogin === true ? (
+            <div class="relative flex items-center md:order-2">
+              <button
+                onClick={handleShow}
+                type="button"
+                class="inline-flex justify-center w-full mx-20 rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+                id="menu-button"
+                aria-expanded="true"
+                aria-haspopup="true"
+              >
+                <span class="sr-only">Open user menu</span>
+                <img
+                  class="w-8 h-8 rounded-full"
+                  src="favicon.ico"
+                  alt="user photo"
+                />
+              </button>
+              {showOptions && (
+                <div
+                  class="absolute top-9 z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow"
+                  id="dropdown"
+                >
+                  <div class="py-3 px-4">
+                    <span class="block text-sm text-gray-900">
+                      Bonnie Green
+                    </span>
+                    <span class="block text-sm font-medium text-gray-500 truncate">
+                      name@flowbite.com
+                    </span>
+                  </div>
+                  <ul class="py-1" aria-labelledby="dropdown">
+                    {drop_item.map((item, index) => NavDropItem(item, index))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          ) : (
+            <>
+              <LoginModal />
+              <RegisterModal />
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
