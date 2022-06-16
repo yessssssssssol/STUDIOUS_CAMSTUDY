b3dc083eb2653bcc79c4b7f45051b15a2cf04a14
kunnie-code <kearnyyoon@gmail.com>
2022-06-16 12:23:56 +0900

    [Fix] upload img logic

--- back/src/routers/userStudyRoomsRouter.js
+++ back/src/routers/userStudyRoomsRouter.js
@@ -8,17 +8,14 @@ import { userStudyRoomsService } from '../services/userStudyRoomsService';
 const userStudyRoomsRouter = Router();
 
 // 스터디룸/게시글 생성
-userStudyRoomsRouter.post('/studyroom', login_required, uploadRoomImgHandler.single('roomImg'), async function (req, res, next) {
+userStudyRoomsRouter.post('/studyroom', login_required, async function (req, res, next) {
     try {
         const id = req.currentUserId;
         const roomId = uuid().replace(/-/g, '');
-        let roomImg = req.file;
         let newRoomInfo = {};
         const now = dayjs();
 
         const { roomName, group, membersOnly, membersNum, startStudyDay, endStudyDay, focusTimeStart, focusTimeEnd, roomTitle, roomDesc, hashTags } = req.body;
-
-        if (roomImg === undefined) roomImg = '사진 정보가 없습니다.';
 
         if (group === false) {
             // 개인 룸
@@ -28,7 +25,7 @@ userStudyRoomsRouter.post('/studyroom', login_required, uploadRoomImgHandler.sin
             newRoomInfo = {
                 id,
                 roomId,
-                roomImg,
+                roomImg: '사진 정보가 없습니다.',
                 roomName,
                 group,
                 startStudyDay,
@@ -94,6 +91,32 @@ userStudyRoomsRouter.post('/studyroom', login_required, uploadRoomImgHandler.sin
         const roomInfo = await userStudyRoomsService.createRoom({ newRoomInfo });
 
         res.status(201).json(roomInfo);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// 스터디룸 사진 저장
+userStudyRoomsRouter.put('/roomimg/:roomId', uploadRoomImgHandler.single('roomImg'), async function (req, res, next) {
+    try {
+        if (!req.file) {
+            res.status(400).json({ message: '업로드할 이미지가 없습니다.' });
+            return;
+        }
+
+        const { roomId } = req.params;
+        const url = req.file.path;
+        console.log(roomId, url);
+        const updateChange = { roomImg: url };
+        const createImg = await userStudyRoomsService.updateRoom({ roomId, updateChange });
+
+        if (!createImg.roomImg || createImg.roomImg === '사진 정보가 없습니다.') {
+            const errorMessage = '이미지가 제대로 저장되지 않았습니다.';
+            console.log(errorMessage);
+            return errorMessage;
+        }
+
+        res.status(200).send({ url: url });
     } catch (error) {
         next(error);
     }
