00e8c09cdad8ba0c8f05054562f9b1a741cd5d4d
ys <lhyahoo0119@gmail.com>
2022-06-17 22:49:53 +0900

    Add dayjs

--- front/core/atoms/aiState.js
+++ front/core/atoms/aiState.js
@@ -1,8 +1,6 @@
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
 
-const { persistAtom } = recoilPersist();
-
 export const aiAtom = atom({
   key: 'userIsHear',
   default: true,
