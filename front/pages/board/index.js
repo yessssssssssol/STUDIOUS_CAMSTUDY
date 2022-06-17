0adc051bcfe2b28e5553ae1a87bd18a5684c6166
vjvl95 <vjvl95@naver.com>
2022-06-17 14:25:38 +0900

    BoardCard Index 완성

--- front/pages/board/index.js
+++ front/pages/board/index.js
@@ -1,105 +1,31 @@
+import BoardCard from '../../components/common/BoardCard';
 import Helmet from '../../components/layout/Helmet';
 
 export default function board(profileURL) {
   return (
-    <div>
+    <div class="flex flex-raw flex-wrap lg:grid grid-rows-3 grid-flow-col gap-4 ">
       <Helmet title="board" />
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="grid gap-5 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
-          <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
-            <a href="/board/detail" aria-label="Article">
-              <img
-                src={profileURL}
-                className="object-cover w-full h-64 rounded"
-                alt="프로필사진"
-              />
-            </a>
-            <div className="py-5">
-              <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
-                13 Jul 2020
-              </p>
-              <a
-                href="/board/detail"
-                aria-label="Article"
-                className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
-              >
-                <p className="text-2xl font-bold leading-5">
-                  공부 같이 하실 분
-                </p>
-              </a>
-              <p className="mb-4 text-gray-700">
-                오전 공부 같이 하실 분 찾습니다!
-              </p>
-              <div className="flex space-x-4">
-                <a
-                  href="/"
-                  aria-label="Likes"
-                  className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
-                >
-                  <div className="mr-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
-                    >
-                      <polyline
-                        points="6 23 1 23 1 12 6 12"
-                        fill="none"
-                        strokeMiterlimit="10"
-                      />
-                      <path
-                        d="M6,12,9,1H9a3,3,0,0,1,3,3v6h7.5a3,3,0,0,1,2.965,3.456l-1.077,7A3,3,0,0,1,18.426,23H6Z"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeMiterlimit="10"
-                      />
-                    </svg>
-                  </div>
-                  <p className="font-semibold">7.4K</p>
-                </a>
-                <a
-                  href="/"
-                  aria-label="Comments"
-                  className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
-                >
-                  <div className="mr-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      stroke="currentColor"
-                      className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
-                    >
-                      <polyline
-                        points="23 5 23 18 19 18 19 22 13 18 12 18"
-                        fill="none"
-                        strokeMiterlimit="10"
-                      />
-                      <polygon
-                        points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeMiterlimit="10"
-                      />
-                    </svg>
-                  </div>
-                  <p className="font-semibold">81</p>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+
+      <BoardCard profileURL={profileURL} />
+      <BoardCard profileURL={profileURL} />
+
+      <BoardCard profileURL={profileURL} />
+
+      <BoardCard profileURL={profileURL} />
+
+      <BoardCard profileURL={profileURL} />
+
+      <BoardCard profileURL={profileURL} />
+
+      <BoardCard profileURL={profileURL} />
+
+      <BoardCard profileURL={profileURL} />
+
+      <BoardCard profileURL={profileURL} />
+
+      <BoardCard profileURL={profileURL} />
+
+      <BoardCard profileURL={profileURL} />
     </div>
   );
 }
