17c7b7112eed8650546e8cae4b578e01759f87fb
ksm9317 <ksm9317@gmail.com>
2022-06-25 13:59:59 +0900

    [Fix]  home 화면 비로그인시 발생하는 에러 수정

--- front/pages/index.js
+++ front/pages/index.js
@@ -3,17 +3,24 @@ import PrologueTop from '../components/prologue/PrologueTop';
 import { isloginAtom } from '../core/atoms/userState';
 import { useRecoilState } from 'recoil';
 import IsLoginHome from '../pages/home/index';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [islogin, setIsLogin] = useRecoilState(isloginAtom);
-  if (!islogin) {
+  const [element, setElement] = useState(null);
+
+  useEffect(() => {
+    setElement(true);
+  }, []);
+
+  if (islogin && element) {
+    return <IsLoginHome />;
+  } else {
     return (
       <div>
         <Helmet title="HOME" />
         <PrologueTop />
       </div>
     );
-  } else {
-    return <IsLoginHome />;
   }
 }
