e026a0e03f28e6fe32c377de09c4b9e8835a1f69
ksm9317 <ksm9317@gmail.com>
2022-06-25 11:08:18 +0900

    [Update] 로그인 여부에 따라 홈화면 전환

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,11 +1,19 @@
 import Helmet from '../components/layout/Helmet';
 import PrologueTop from '../components/prologue/PrologueTop';
+import { isloginAtom } from '../core/atoms/userState';
+import { useRecoilState } from 'recoil';
+import IsLoginHome from '../pages/home/index';
 
 export default function Home() {
-  return (
-    <div>
-      <Helmet title="HOME" />
-      <PrologueTop />
-    </div>
-  );
+  const [islogin, setIsLogin] = useRecoilState(isloginAtom);
+  if (!islogin) {
+    return (
+      <div>
+        <Helmet title="HOME" />
+        <PrologueTop />
+      </div>
+    );
+  } else {
+    return <IsLoginHome />;
+  }
 }
