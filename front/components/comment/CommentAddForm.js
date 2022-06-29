68437906d0c4f4260e7e9c39ad144c30c00f9e77
Hailey_ys <whassup_man@naver.com>
2022-06-29 18:32:49 +0900

    Fix commentCard img

--- front/components/comment/CommentAddForm.js
+++ front/components/comment/CommentAddForm.js
@@ -30,7 +30,7 @@ const CommentAddForm = ({ roomId, setComments, writerId }) => {
   return (
     <>
       {user && (
-        <div className="my-2 mx-1 max-w-xl flex gap-3 rounded-md bg-white p-2 text-black shadow">
+        <div className="my-2 mx-1 max-w-l flex gap-3 rounded-md bg-white p-2 text-black shadow">
           <div className="mt-2">
             <img
               src={user.profileUrl}
@@ -41,23 +41,23 @@ const CommentAddForm = ({ roomId, setComments, writerId }) => {
           <div className="flex">
             <div className="flex flex-row items-center justify-between py-1 pr-2">
               <div>
-                <a href="#" className="text-blue-400 hover:underline">
+                <a href="#" className="text-amber-400 hover:underline">
                   {user.name}
                 </a>
               </div>
               <div className="p-1">
-                <textarea
+                <input
                   rows="3"
-                  className="border p-2 rounded w-full"
-                  placeholder="Write something..."
+                  className="border p-2 rounded w-full mx-5"
+                  placeholder="댓글을 입력하세요."
                   value={content}
                   onChange={(e) => setContent(e.target.value)}
-                ></textarea>
+                ></input>
               </div>
               <div className="flex justify-between mx-3">
                 <div>
                   <button
-                    className="px-4 py-1 bg-gray-800 text-white rounded font-light hover:bg-gray-700"
+                    className="px-4 py-1 bg-amber-400 text-white rounded font-light hover:bg-amber-500 hover:shadow-lg mx-5"
                     type="submit"
                     onClick={handleSubmit}
                   >
