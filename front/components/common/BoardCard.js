abdf9e087dc8bddb090b02f90fbd41e5d015ca91
Hailey_ys <whassup_man@naver.com>
2022-06-29 21:15:20 +0900

    Fix board card width

--- front/components/common/BoardCard.js
+++ front/components/common/BoardCard.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 
 export default function BoardCard({ boardData, profileURL }) {
   return (
-    <div className="px-2 py-16 mx-auto sm:max-w-xl  md:px-12 lg:px-8 lg:py-20">
+    <div className="w-96 px-2 py-16 mx-auto sm:max-w-xl  md:px-12 lg:px-8 lg:py-20">
       <div className="grid gap-5 sm:max-w-sm sm:mx-auto">
         <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
           <a href={`/board/detail/${boardData.roomId}`}>
