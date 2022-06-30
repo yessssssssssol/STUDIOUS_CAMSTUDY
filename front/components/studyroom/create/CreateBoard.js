58881282cec7075ddd86da56044772ebe535dbab
Hailey_ys <whassup_man@naver.com>
2022-07-01 00:20:38 +0900

    Fix Edit/Create CSS

--- front/components/studyroom/create/CreateBoard.js
+++ front/components/studyroom/create/CreateBoard.js
@@ -29,24 +29,12 @@ const CreateBoard = () => {
   };
 
   return (
-    <div className="container mx-auto bg-white rounded">
-      <div className="xl:w-full border-b border-gray-300  py-3 bg-white ">
+    <div className="container mx-72 bg-white rounded">
+      <div className="xl:w-full border-b border-amber-400  py-3 bg-white ">
         <div className="flex w-11/12 mx-auto xl:w-full xl:mx-0 items-center">
-          <p className="text-lg text-gray-800  font-bold">스터디원 모집</p>
-          <div className="ml-2 cursor-pointer text-gray-600 ">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="16"
-              height="16"
-            >
-              <path
-                className="heroicon-ui"
-                d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-9a1 1 0 0 1 1 1v4a1 1 0 0 1-2 0v-4a1 1 0 0 1 1-1zm0-4a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
-                fill="currentColor"
-              />
-            </svg>
-          </div>
+          <p className="text-2xl text-amber-400  font-bold">
+            스터디원 모집 게시글
+          </p>
         </div>
       </div>
       <div className="mx-auto">
@@ -63,14 +51,14 @@ const CreateBoard = () => {
               id="title"
               value={roomTitle}
               onChange={onTitleChange}
-              className="border border-gray-300  pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-indigo-700 bg-transparent placeholder-gray-500 text-gray-600 "
+              className="border border-gray-300  pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-amber-400 bg-transparent placeholder-gray-500 text-gray-600 "
               placeholder="스터디 모집 합니다."
             />
           </div>
           <div className="mt-8 flex flex-col xl:w-3/5 lg:w-1/2 md:w-1/2 w-full">
             <label
               htmlFor="about"
-              className="pb-2 text-sm font-bold text-gray-800 "
+              className="pb-2 text-sm font-bold text-gray-800  "
             >
               내용
             </label>
@@ -80,7 +68,7 @@ const CreateBoard = () => {
               value={roomDesc}
               onChange={onDescChange}
               required
-              className="bg-transparent border border-gray-300  pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-indigo-700 resize-none placeholder-gray-500 text-gray-600 "
+              className="bg-transparent border border-gray-300  pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-amber-400 resize-none placeholder-gray-500 text-gray-600 "
               placeholder="스터디 모집에 관한 자세한 이야기를 적어주세요!"
               rows="5"
             ></textarea>
@@ -96,7 +84,7 @@ const CreateBoard = () => {
           <input
             type="text"
             id="name"
-            className="bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            className="bg-white rounded border border-gray-300 focus:border-amber-400 focus:ring-2  text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             placeholder="ex)#프론트 #백앤드"
             value={tag}
             onChange={(e) => setHashTag(e.target.value)}
