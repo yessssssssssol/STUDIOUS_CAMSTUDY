c1c7b077240f881c07c4001d4a66f42639c9e286
ys <lhyahoo0119@gmail.com>
2022-06-30 11:37:08 +0900

    Update 퇴장, 신청시 component 바로 렌더링

--- front/components/common/Button.js
+++ front/components/common/Button.js
@@ -1,8 +1,9 @@
-export default function Button({ text, onClick }) {
+export default function Button({ text, onClick, disable }) {
   return (
     <button
       onClick={onClick}
       className="bg-amber-400 hover:shadow-lg hover:bg-amber-500 text-white font-semibold py-1 px-3 rounded-md"
+      disable={disable}
     >
       {text}
     </button>
