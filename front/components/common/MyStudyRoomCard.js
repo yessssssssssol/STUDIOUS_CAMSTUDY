849b6afa1ccf1e576d84ad34142783155b0b944f
Hailey_ys <whassup_man@naver.com>
2022-06-30 21:54:52 +0900

    Fix home card padding margin

--- front/components/common/MyStudyRoomCard.js
+++ front/components/common/MyStudyRoomCard.js
@@ -14,8 +14,8 @@ export default function MyStudyRoomCard({ myStudyRoom }) {
                   alt="Rounded avatar"
                 />
               </div>
-              <div className="absolute top-5 left-5 px-6 py-4">
-                <p className="text-3xl text-black font-bold">
+              <div className="absolute top-10 left-5 px-6 py-4">
+                <p className="text-2xl text-black font-bold">
                   {'개인 스터디룸'}
                 </p>
                 <div className="flex space-x-4"></div>
@@ -29,7 +29,7 @@ export default function MyStudyRoomCard({ myStudyRoom }) {
             aria-label="Article"
           >
             {/* <!-- card --> */}
-            <div className="mx-auto flex h-[300px] w-[250px] flex-col justify-center bg-gradient-to-r from-amber-50 to-grag-50 rounded-2xl shadow-xl shadow-slate-300/60">
+            <div className="mx-auto flex h-[300px] w-[250px] flex-col justify-center bg-gradient-to-r rounded-2xl shadow-xl shadow-slate-300/60">
               {/* <div className="mx-auto flex h-[300px] w-[250px] flex-col justify-center bg-gray-200 rounded-2xl shadow-xl shadow-slate-300/60"> */}
               {/* <!-- img --> */}
               <img
@@ -39,7 +39,7 @@ export default function MyStudyRoomCard({ myStudyRoom }) {
               />
               {/* <!-- text information --> */}
               <div className="px-4">
-                <small className="text-amber-400 text-xs font-semibold">
+                <small className="text-amber-400 text-sx font-semibold">
                   {myStudyRoom.members.length + ' / ' + myStudyRoom.membersNum}
                 </small>
                 {/* 제목 클릭하면 스터디룸 게시판(?) 연결  */}
@@ -49,7 +49,7 @@ export default function MyStudyRoomCard({ myStudyRoom }) {
                     href={`/board/detail/${myStudyRoom.roomId}`}
                     aria-label="Article"
                   >
-                    <p className="text-2xl font-bold  text-slate-600 pb-2 text-center">
+                    <p className="text-2xl font-bold  text-slate-600 pb-2">
                       {myStudyRoom.roomName}
                     </p>
                   </a>
@@ -59,12 +59,12 @@ export default function MyStudyRoomCard({ myStudyRoom }) {
                     href={`/openroom/board/${myStudyRoom.roomId}`}
                     aria-label="Article"
                   >
-                    <p className="text-2xl font-bold text-slate-600 pb-2 text-center">
+                    <p className="text-2xl font-bold text-slate-600 pb-2 ">
                       {myStudyRoom.roomName}
                     </p>
                   </a>
                 )}
-                <p className="text-sm font-medium text-slate-400 text-center pb-2 ">
+                <p className="text-sm font-medium text-slate-400 pb-2 ">
                   {myStudyRoom.focusTimeStart.slice(0, 5) +
                     ' ~ ' +
                     myStudyRoom.focusTimeEnd.slice(0, 5)}
--- front/components/common/OpenroomCard.js
+++ front/components/common/OpenroomCard.js
@@ -1,29 +1,29 @@
 export default function OpenroomCard({ openRoom }) {
   return (
-    <div className="mx-auto flex h-[300px] w-64 justify-center bg-gradient-to-r hover:scale-105 duration-300 rounded-md hover:shadow-amber-300/50 shadow-lg">
-      <div className="grid sm:max-w-sm sm:mx-auto">
-        <div className="bg-white rounded-md m-0">
-          <a href={`/openroom/board/${openRoom.roomId}`} aria-label="Article">
+    <div className="w-80 h-full px-2 py-16 mx-auto sm:max-w-xl md:px-2 lg:px-8 lg:py-8">
+      <div className="grid gap-5 sm:max-w-sm sm:mx-auto">
+        <a href={`/openroom/board/${openRoom.roomId}`} aria-label="Article">
+          <div className="over-flow-hidden bg-white rounded-md hover:scale-105 hover:shadow-amber-300/50 shadow-lg duration-300">
             <img
-              className="w-64 h-48 object-fill rounded-t-md"
+              className="w-full h-48 object-fill rounded-t-md"
               src={openRoom.roomImg}
               alt="Rounded avatar"
             />
-          </a>
-          <div className="px-4">
-            <small className="text-amber-400 font-bold">
-              {openRoom.members.length + ' / ' + openRoom.membersNum}
-            </small>
-            <p className="text-2xl font-bold text-center text-slate-600 pb-2">
-              {openRoom.roomName}
-            </p>
-            <p className="text-sm font-normal text-center text-slate-400">
-              {openRoom.focusTimeStart.slice(0, 5) +
-                ' ~ ' +
-                openRoom.focusTimeEnd.slice(0, 5)}
-            </p>
+            <div className="py-5 px-4">
+              <small className="text-amber-400 font-bold">
+                {openRoom.members.length + ' / ' + openRoom.membersNum}
+              </small>
+              <p className="text-2xl font-bold  text-black pb-2">
+                {openRoom.roomName}
+              </p>
+              <p className="text-sm font-normal  text-slate-400">
+                {openRoom.focusTimeStart.slice(0, 5) +
+                  ' ~ ' +
+                  openRoom.focusTimeEnd.slice(0, 5)}
+              </p>
+            </div>
           </div>
-        </div>
+        </a>
       </div>
     </div>
   );
