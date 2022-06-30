09d1aa654a2a979e25cff8b2adfb924c418665c3
kunnie-code <kearnyyoon@gmail.com>
2022-06-30 17:38:40 +0900

    [Fix] add setTimeout

--- front/components/common/RankingTable2.js
+++ front/components/common/RankingTable2.js
@@ -2,49 +2,74 @@ import { useState } from 'react';
 
 export default function RankingTable({ rankings, userDatas }) {
   const [open, setOpen] = useState(false);
+  const [done, setDone] = useState(true);
 
-  const rankChart = rankings.map((ranking, index) => {
-    const user = userDatas.find((userData) => userData.id === ranking.user_id);
-    const css =
-      'flex flex-row h-14 items-center mx-10 my-3 mb-2 rounded-xl shadow shadow-amber-700/20';
-    let color = undefined;
-    let medal = undefined;
-    if (index === 0) {
-      color = ' bg-amber-100';
-      medal = 'gold.png';
-    } else if (index === 1) {
-      color = ' bg-amber-50';
-      medal = 'silver.png';
-    } else if (index === 2) {
-      color = ' bg-amber-50';
-      medal = 'bronze.png';
+  function handleDone() {
+    if (open) {
+      setTimeout(() => {
+        setDone((bool) => !bool);
+      }, 800);
     } else {
-      color = ' bg-amber-50';
-      medal = 'noneMedal.png';
+      setDone((bool) => !bool);
+    }
+  }
+
+  function rankAr() {
+    let margin = undefined;
+    if (done === false) {
+      margin = 'mb-2';
+    } else {
+      margin = 'mb-14';
     }
 
-    return (
-      <div key={index} className={css + color}>
-        <div className="font-semibold basis-1/12 text-center">{index + 1}</div>
-        <div className="font-semibold basis-1/12 text-center flex justify-center items-center">
-          <img className="w-10 h-10 mr-5" src={medal}></img>
-          <img
-            className="rounded-full bg-amber-400 w-10 h-10"
-            src={user?.profileUrl}
-          />
+    const rankChart = rankings.map((ranking, index) => {
+      const user = userDatas.find(
+        (userData) => userData.id === ranking.user_id
+      );
+      const css =
+        'flex flex-row h-14 items-center mx-10 my-3 rounded-xl shadow shadow-amber-700/20';
+      let color = undefined;
+      let medal = undefined;
+      if (index === 0) {
+        color = ' bg-amber-100';
+        medal = 'gold.png';
+      } else if (index === 1) {
+        color = ' bg-amber-50';
+        medal = 'silver.png';
+      } else if (index === 2) {
+        color = ' bg-amber-50';
+        medal = 'bronze.png';
+      } else {
+        color = ' bg-amber-50';
+        medal = 'noneMedal.png';
+      }
+
+      return (
+        <div key={index} className={`${css} ${color} ${margin}`}>
+          <div className="font-semibold basis-1/12 text-center">
+            {index + 1}
+          </div>
+          <div className="font-semibold basis-1/12 text-center flex justify-center items-center">
+            <img className="w-10 h-10 mr-5" src={medal}></img>
+            <img
+              className="rounded-full bg-amber-400 w-10 h-10"
+              src={user?.profileUrl}
+            />
+          </div>
+          <div className="font-semibold basis-2/12 pl-10 truncate">
+            {user?.name}
+          </div>
+          <div className="basis-5/12 text-center ml-10 truncate">
+            {user?.description}
+          </div>
+          <div className="font-semibold basis-3/12 text-center pl-10 truncate">
+            {ranking.totalTime}
+          </div>
         </div>
-        <div className="font-semibold basis-2/12 pl-10 truncate">
-          {user?.name}
-        </div>
-        <div className="basis-5/12 text-center ml-10 truncate">
-          {user?.description}
-        </div>
-        <div className="font-semibold basis-3/12 text-center pl-10 truncate">
-          {ranking.totalTime}
-        </div>
-      </div>
-    );
-  });
+      );
+    });
+    return rankChart;
+  }
 
   //여기서 부터 렌더링입니다.
   return (
@@ -67,13 +92,16 @@ export default function RankingTable({ rankings, userDatas }) {
         <div className="animate-bounce ">
           <button
             className={`mt-2 duration-700 ${open ? 'rotate-180' : ''} `}
-            onClick={() => setOpen((bool) => !bool)}
+            onClick={() => {
+              setOpen((bool) => !bool);
+              handleDone();
+            }}
           >
             <img className="w-8 h-8" src="/dropDown.png"></img>
           </button>
         </div>
       </div>
-      {rankChart}
+      {rankAr()}
       {/* 내 랭킹 보기 기능도 추후에 추가할 수 있도록 만들었싐미다 -여건-*/}
       {/* <div className="flex flex-row h-14 items-center mx-10 my-10 bg-amber-50 mb-2 rounded-xl shadow shadow-amber-700/20  ">
         <div className="font-semibold basis-1/12 text-center">33</div>
