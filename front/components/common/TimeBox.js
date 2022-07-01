79c587fc1d8e66282d397bf587b0021c2feba2a8
Hailey_ys <whassup_man@naver.com>
2022-07-01 19:05:32 +0900

    Fix timeBox

--- front/components/common/TimeBox.js
+++ front/components/common/TimeBox.js
@@ -1,7 +1,7 @@
 export default function TimeBox({ timeData, index, timeGoal }) {
   const title = ['오늘 공부 시간', '이번주 공부 시간', '전체 공부 시간'];
   return (
-    <div className="flex-col box-border w-80 h-40 p-4 space-y-3 border-2 border-amber-400 shadow-xl rounded-xl">
+    <div className="flex-col box-border w-80 h-40 p-4 m-4 space-y-3 border-2 justify-evenly border-amber-400 shadow-xl rounded-xl">
       <div className="text-xl font-semibold text-gray-400 capitalize text-center">
         {title[index]}
       </div>
